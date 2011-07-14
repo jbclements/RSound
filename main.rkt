f92ca4d75b5da0e29a3ac4ee9f9f39c3bc4f43b9
John Clements <clements@racket-lang.org>
2011-07-14 15:23:15 -0700

    removed tool/drracket stuff

--- main.rkt
+++ main.rkt
@@ -1,13 +1,8 @@
 #lang racket
 
-(require (prefix-in link: "private/drracket-link.rkt")
-         "private/portaudio.rkt"
+(require "private/portaudio.rkt"
          "rsound.rkt"
          "util.rkt")
-
-;; if init hasn't happened, do it now.
-(when (semaphore-try-wait? link:init-semaphore)
-  (pa-initialize))
 
 
 (provide (all-from-out "rsound.rkt")
