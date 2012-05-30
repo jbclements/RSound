5ea6c9cf1a0ba9bfd8f3138de39e586217d27036
John Clements <clements@racket-lang.org>
2012-05-29 17:21:21 -0700

    added quantizer

---
+++ examples/gogo.rkt
@@ -0,0 +1,89 @@
+#lang racket
+
+;; hastily assembled demo for Foaad Khosmood's class
+
+(require (planet clements/rsound)
+         (planet clements/rsound/draw)
+         "quantize-looper.rkt"
+         ffi/vector
+         
+         2htdp/universe
+         2htdp/image)
+
+(define song-chunk
+  (rs-read/clip
+   "/Users/clements/clements/teaching/video-game-music/earworm-2011.wav"
+   0 (* 44100 30)))
+
+(go song-chunk)
+
+(define (my-draw w)
+  (empty-scene 320 320))
+
+(define key-table (map (lambda (ch) (string ch))
+                       (string->list "123456789")))
+(define t-key-table (map (lambda (ch) (string ch))
+                         (string->list "',.pyfgcrl")))
+
+(define (my-key w k)
+  (cond [(equal? k "x")
+         (stop)]
+        [(member k key-table) 
+         (update-q! (sub1 (string->number k)))]
+        [(member k t-key-table)
+         =>
+         (lambda (k)
+           (update-time-q! (- (length t-key-table) (length k))))])
+  w)
+
+(big-bang #f
+          (to-draw my-draw)
+          (on-key my-key))
+
+#|
+#;(play song-chunk)
+
+(define msr mono-signal->rsound)
+
+(define (quantize q num)
+  (/ (/ (floor (* q (* num 32768.0))) q) 32768.0))
+
+;; given a sound and a multiplier, do "fake resampling"
+;; by choosing a rounded sample from the original
+
+;; given a factor and a sound, resample the sound (using simple rounding)
+;; to obtain a new one. Using e.g. factor of 2 will make the sound one
+;; octave higher and half as long.
+(define (my-resample sound new-freq q)
+  (define factor (let ([ans (/ new-freq (rsound-sample-rate sound))])
+                   (printf "~s\n" ans)
+                   ans))
+  (define (left i) (quantize 
+                    q
+                    (rs-ith/left sound 
+                                 (inexact->exact (floor 
+                                                  (/ (floor (* factor i))
+                                                     factor))))))
+  (define (right i) (quantize
+                     q
+                     (rs-ith/right sound 
+                                   (inexact->exact (floor
+                                                    (/ (floor (* factor i)) 
+                                                       factor))))))
+  (parameterize ([default-sample-rate 
+                   (rsound-sample-rate sound)])
+    (signals->rsound (rs-frames sound)
+                     left
+                     right)))
+
+
+(define start-clip (* 10 44100))
+
+(rs-draw (clip song-chunk start-clip (+ start-clip 500)))
+
+(define s (my-resample song-chunk 44100 1.0))
+
+(rs-draw (clip s start-clip (+ start-clip 500)))
+(play s)
+
+|#
---
+++ examples/quantize-looper.rkt
@@ -0,0 +1,80 @@
+#lang racket
+
+;; hastily assembled demo for Foaad Khosmood's class
+
+(require (planet clements/rsound)
+         (planet clements/rsound/paste-util)
+         ffi/unsafe
+         ffi/vector)
+
+(provide go
+         update-q!
+         update-time-q!)
+
+;; go : (listof rsound) -> (void)
+(define (go sound)
+  (set! stored-snd sound)
+  (signal/block-play/unsafe copier 44100))
+
+(define (update-q! num)
+  (define new-val (vector-ref quantize-vec num))
+  (printf "quantized to ~s levels\n" (exact->inexact (/ 65536 new-val)))
+  (set! quantize new-val)
+  (print-stats))
+
+(define (update-time-q! num)
+  (define new-val (vector-ref time-quantize-vec num))
+  (printf "quantized to sample rate of ~s\n" (exact->inexact (/ 44100 new-val)))
+  (set! time-quantize new-val)
+  (print-stats))
+
+(define (print-stats)
+  (define levels (/ 65536 quantize))
+  (define sr (/ 44100 time-quantize))
+  (define bytes-per-sec (* sr (/ (/ (log levels) (log 2)) 8)))
+  (printf "bytes per second: ~s\n" (exact->inexact bytes-per-sec)))
+
+;; update-play-toggles : (vectorof boolean) -> void
+#;(define (update-play-toggles! new-vec)
+  (unless (= (vector-length new-vec)
+             (length stored-snds))
+    (error 'update-play-toggles "wrong length vector, must be ~s"
+           (length stored-snds)))
+  (set! sound-toggles new-vec))
+
+;; toggle-loop! : number -> void
+;; turn on or off a particular loop
+#;(define (toggle-loop! n)
+  (define entry-box (vector-ref (vector-ref snd-table n) 1))
+  (set-box! entry-box (not (unbox entry-box))))
+
+(define stored-snd #f)
+(define quantize 1)
+(define time-quantize 1)
+(define frames-offset 0)
+
+(define quantize-vec '#(1 4 16 64 256 1024 4096 8192 16384 32768))
+(define time-quantize-vec '#(1 4 16 64 128 256 512 1024))
+
+
+
+(define (copier buf len frame)
+  (define snd-vec (rsound-data stored-snd))
+  (define snd-len (rs-frames stored-snd))
+  (define frame-to-start-copying (- frame frames-offset))
+  (define frame-to-stop-copying (- (+ frame-to-start-copying len) frames-offset))
+  (cond [(< snd-len frame-to-stop-copying)
+         ;; go back to beginning: 
+         (set! frames-offset frame)
+         ;; ... and try again
+         (copier buf len frame)]
+        [else
+         (for* ([i (in-range (* 2 len))])
+           (define j (+ i (* 2 (- frame frames-offset))))
+           (define qj (inexact->exact (* time-quantize (floor (/ j time-quantize)))))
+           (define samp (inexact->exact 
+                         (min (max -32768 (* quantize (round (/ (s16vector-ref snd-vec qj) quantize))))
+                              32767)))
+           (ptr-set! buf _sint16 i samp))]))
+
+
