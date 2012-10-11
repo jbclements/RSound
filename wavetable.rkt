8c521a59e87bf833b57f603690f0a94bb92fdf89
John Clements <clements@racket-lang.org>
2012-10-11 11:22:41 -0700

    rolled back wavetable.rkt

--- wavetable.rkt
+++ wavetable.rkt
@@ -6,33 +6,33 @@
 (provide 
  (contract-out [build-wavetable (-> procedure? 
                                     flvector?)]
+               [wavetable-build-sample-rate number?]
                [make-table-based-wavefun
                 (-> flvector? (-> number? number? signal?))]))
 
-(define SR (inexact->exact (default-sample-rate)))
-(unless (integer? SR)
-  (raise-argument-error 'build-wavetable "integer" 1 fun SR))
-
 ;; build a wavetable for a periodic function with the 
 ;; contract (pitch sample-rate -> signal)
-(define (build-wavetable fun SR)
-
-  (define srint (inexact->exact SR))
-  (define sigfun (network-init (fun 1)))
-  (let ([newvec (make-flvector srint)])
-    (for ([i (in-range srint)])
+(define (build-wavetable fun)
+  (define sigfun (network-init (fun 1 wavetable-build-sample-rate)))
+  (let ([newvec (make-flvector wavetable-build-sample-rate)])
+    (for ([i (in-range wavetable-build-sample-rate)])
       (flvector-set! newvec i (exact->inexact (sigfun))))
     newvec))
 
 ;; given a wavetable, make a wavetable lookup function
 ;; how much slower would it be with interpolation?
 ;; flvector -> number number -> signal
-(define ((make-table-based-wavefun vec) pitch)
-  (define skip-rate (inexact->exact (round pitch)))
+(define ((make-table-based-wavefun vec) pitch sample-rate)
+  (define relative-pitch (* pitch (/ wavetable-build-sample-rate sample-rate)))
+  (define skip-rate (inexact->exact (round relative-pitch)))
   (define index-net (loop-ctr wavetable-build-sample-rate skip-rate))
   (network ()
            (idx (index-net))
            (out (flvector-ref vec idx))))
+
+;; this is independent, but it should be nice and high to get 
+;; good wavetables
+(define wavetable-build-sample-rate 44100)
 
 (module+ test
   (require rackunit)
