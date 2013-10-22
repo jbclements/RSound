ecc81b9dce12ffdfe72adca883a95da861261676
John Clements <clements@racket-lang.org>
2013-10-22 15:26:20 -0700

    updated deps

--- info.rkt
+++ info.rkt
@@ -1,4 +1,14 @@
 #lang setup/infotab
 
 (define collection 'multi)
-(define deps (list "portaudio"))
+(define deps (list "portaudio"
+                   "base"
+                   "data-lib"
+                   "gui-lib"
+                   "htdp-lib"
+                   "plot-gui-lib"
+                   "rackunit-lib"
+                   "scheme-lib"
+                   "typed-racket-lib"))
+(define build-deps (list "racket-doc"
+                         "scribble-lib"))
