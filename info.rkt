0380c049f0fcbef244a6d320ed5e27f81de2bddb
John Clements <clements@racket-lang.org>
2013-10-24 22:19:25 -0700

    temporarily remove dependencies

--- info.rkt
+++ info.rkt
@@ -1,14 +1,20 @@
 #lang setup/infotab
 
 (define collection 'multi)
+
+;; unfortunately, this doesn't work in 5.3.6.... even
+;; though the catalog supplies dummy versions, the
+;; graphical tool pops up a Y/N message in a dialog
+;; that has no means of entering text!
 (define deps (list "portaudio"
-                   "base"
-                   "data-lib"
-                   "gui-lib"
-                   "htdp-lib"
-                   "plot-gui-lib"
-                   "rackunit-lib"
-                   "scheme-lib"
-                   "typed-racket-lib"))
-(define build-deps (list "racket-doc"
-                         "scribble-lib"))
+                   ;"base"
+                   ;"data-lib"
+                   ;"gui-lib"
+                   ;"htdp-lib"
+                   ;"plot-gui-lib"
+                   ;"rackunit-lib"
+                   ;"scheme-lib"
+                   ;"typed-racket-lib"
+                   ))
+;(define build-deps (list "racket-doc"
+;                         "scribble-lib"))
